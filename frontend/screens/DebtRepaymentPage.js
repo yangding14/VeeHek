c84c8eb849b8d07dd547f492329d9f86fbcaca4d
Shawn Chee <aykaychee2@gmail.com>
2024-03-23 02:58:55 +0800

    Done DebtRepaymentPage
    easuy

--- frontend/screens/DebtRepaymentPage.js
+++ frontend/screens/DebtRepaymentPage.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Text, StyleSheet, View, Image ,TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import { TextInput ,Text, StyleSheet, View, Image ,TouchableOpacity, Modal} from 'react-native';
 import {appStyles, colors, fonts, sh, sw} from '../styles/GlobalStyle';
 import DebtRepaymentBox from '../components/DebtRepaymentBox';
 
 function DebtRepaymentPage(props) {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+    }
+    
     return (
         <View style= {appStyles.appBackground}>
             <View style = {{flexDirection: 'row', alignItems: 'center',gap: sw(20), alignSelf:'center'}}>
@@ -18,10 +24,27 @@ function DebtRepaymentPage(props) {
             <TouchableOpacity style = {debtPage.emailButton}>
             <Text style= {{fontFamily: fonts.PoppinsSemiBold, fontSize: sh(16)}}>Email a Copy</Text>
             </TouchableOpacity>
-            <TouchableOpacity style = {debtPage.saveButton}>
+            <TouchableOpacity onPress= {toggleModal} style = {debtPage.saveButton}>
             <Text style= {{fontFamily: fonts.PoppinsSemiBold, fontSize: sh(16)}}>Save</Text>
             </TouchableOpacity>
-            
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(false);
+                }}
+                >
+                <View style= {debtPage.modalBox}>
+                    <Text style = {{fontFamily: fonts.PoppinsSemiBold, fontSize: sh(24)}}>Name this plan:</Text>
+                    <TextInput style= {debtPage.textInput}/>
+                    <TouchableOpacity onPress= {toggleModal} style = {debtPage.saveButton}>
+                    <Text style= {{fontFamily: fonts.PoppinsSemiBold, fontSize: sh(16)}}>Save</Text>
+                    {/* SAVE FUNCTION HERE */}
+                    </TouchableOpacity>
+                </View>
+                
+                </Modal>
         </View>
     );
 }
@@ -58,7 +81,8 @@ const debtPage = StyleSheet.create({
         alignItems: 'center',
         paddingVertical: sh(10),
         paddingHorizontal: sw(15),
-        width: sw(150),  },
+        width: sw(150),  
+    },
 
     saveButton : {
         alignSelf: 'center',
@@ -72,6 +96,32 @@ const debtPage = StyleSheet.create({
         paddingVertical: sh(10),
         paddingHorizontal: sw(15),
         width: sw(150),
+    },
+
+    modalBox : {
+        borderWidth:3,
+        borderColor: '#3F9A43' ,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.greenLight,
+        borderRadius: sh(20),
+        padding: sh(20),
+        marginHorizontal: sw(20),
+        marginTop: sh(300),
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 4},   
+        shadowOpacity: 0.55,
+    },
+
+    textInput : {
+        borderRadius: sh(10),
+        marginTop: sh(10),
+        backgroundColor: "#D9D9D9",
+        color: colors.greyBlue,
+        height: sh(50),
+        width: sw(200), 
+        padding: sh(5),
+        marginBottom: sh(20)
     }
 })
 
